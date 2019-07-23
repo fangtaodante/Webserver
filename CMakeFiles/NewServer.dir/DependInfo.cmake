c1faed45feb5c8dd9a3de12fb3b83688aa37d118
fangtaodante <fangtao_dante@126.com>
2019-07-23 16:54:15 +0800

    nothing

---
+++ CMakeFiles/NewServer.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/dante/Projects/Jenkins/CIWorkSpace/Webserver/main.c" "/home/dante/Projects/Jenkins/CIWorkSpace/Webserver/CMakeFiles/NewServer.dir/main.c.o"
+  "/home/dante/Projects/Jenkins/CIWorkSpace/Webserver/server_epoll.c" "/home/dante/Projects/Jenkins/CIWorkSpace/Webserver/CMakeFiles/NewServer.dir/server_epoll.c.o"
+  "/home/dante/Projects/Jenkins/CIWorkSpace/Webserver/wrap.c" "/home/dante/Projects/Jenkins/CIWorkSpace/Webserver/CMakeFiles/NewServer.dir/wrap.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
